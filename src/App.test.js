129cb1467e7e13e67ba0ba4d7e06fcaf03f6ec1a
Nikolay Tomitov <ntomitov@gmail.com>
2022-07-27 10:36:48 +0300

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,10 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
